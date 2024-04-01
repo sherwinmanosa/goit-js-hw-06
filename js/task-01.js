586e6be3448cdf71379e7e010b0de5d43ce0b1a2
sherwinmanosa <sherwin.manosa@gmail.com>
2024-04-01 10:22:55 +0800

    rev00

---
+++ js/task-01.js
@@ -0,0 +1,9 @@
+const categories = document.querySelectorAll(".item");
+console.log(`Number of categories: ${categories.length}`);
+
+for (let category of categories) {
+  console.log(`Category: ${category.firstElementChild.innerText}`);
+  console.log(
+    `Elements: ${category.firstElementChild.nextElementSibling.children.length}`
+  );
+}
